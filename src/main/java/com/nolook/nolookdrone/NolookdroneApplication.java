dff0d52d92f8c057d8e2bd986b877d2774e5a52a
SeungGun <saverrw2864@naver.com>
2023-06-26 02:09:38 +0900

    chore: spring project initialized

---
+++ src/main/java/com/nolook/nolookdrone/NolookdroneApplication.java
@@ -0,0 +1,13 @@
+package com.nolook.nolookdrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolookdroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolookdroneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nolook/nolookdrone/NolookdroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolook.nolookdrone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolookdroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
