03dcf92708ea4ae31e663e4d28994abd1db3f219
Hyunstone <hyun104467@naver.com>
2023-07-04 16:18:22 +0900

    refactor: inputValidation regex 리팩토링 진행

--- src/main/java/com/nolook/nolookdrone/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/nolook/nolookdrone/domain/user/service/UserServiceImpl.java
@@ -16,13 +16,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Boolean inputValidation(UserCreateDto userCreateDto) {
-        if (!(userCreateDto.getUserId().matches(Regex.userIdRegex.getRegex()) &&
-                userCreateDto.getPhone().matches(Regex.phoneRegex.getRegex()) &&
-                userCreateDto.getUserPassword().matches(Regex.passwordRegex.getRegex()) &&
-                userCreateDto.getEmail().matches(Regex.emailRegex.getRegex()))) {
-            return false;
-        }
-
-        return true;
+//        if (!(
+//                userCreateDto.getUserId().matches(Regex.USER_ID_REGEX.getRegex()) &&
+//                userCreateDto.getPhone().matches(Regex.PHONE_REGEX.getRegex()) &&
+//                userCreateDto.getUserPassword().matches(Regex.USER_PASSWORD_REGEX.getRegex()) &&
+//                userCreateDto.getEmail().matches(Regex.EMAIL_REGEX.getRegex()))) {
+//            return false;
+//        }
+//        return true;
+        // 기존 코드는 조건에서 !을 붙어 조건 해석시에 실수 방지 및 코드 리팩토링 진행
+        return userCreateDto.matchRegex(userCreateDto.getUserId(), Regex.USER_ID_REGEX.getRegex()) &&
+                userCreateDto.matchRegex(userCreateDto.getUserPassword(), Regex.USER_PASSWORD_REGEX.getRegex()) &&
+                userCreateDto.matchRegex(userCreateDto.getEmail(), Regex.EMAIL_REGEX.getRegex()) &&
+                userCreateDto.matchRegex(userCreateDto.getPhone(), Regex.PHONE_REGEX.getRegex());
     }
 }
--- src/main/java/com/nolook/nolookdrone/global/common/Regex.java
+++ src/main/java/com/nolook/nolookdrone/global/common/Regex.java
@@ -1,10 +1,10 @@
 package com.nolook.nolookdrone.global.common;
 
 public enum Regex {
-    phoneRegex("^\\d{2,3}\\d{3,4}\\d{4}$"),
-    userIdRegex("^[a-zA-Z0-9]*$"),
-    passwordRegex("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{1,}$"),
-    emailRegex("[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$");
+    PHONE_REGEX("^\\d{2,3}\\d{3,4}\\d{4}$"),
+    USER_ID_REGEX("^[a-zA-Z0-9]*$"),
+    USER_PASSWORD_REGEX("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{1,}$"),
+    EMAIL_REGEX("[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$");
 
     private String regex;
     Regex(String regex) {
