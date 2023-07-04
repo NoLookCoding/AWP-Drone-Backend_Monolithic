bf7b84caa6f0e0e425c98471b1035e6bdd3e2afc
Hyunstone <hyun104467@naver.com>
2023-07-04 16:02:22 +0900

    fix: UserCreateDto

--- src/main/java/com/nolook/nolookdrone/domain/user/controller/UserController.java
+++ src/main/java/com/nolook/nolookdrone/domain/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.nolook.nolookdrone.domain.user.controller;
 
+import com.nolook.nolookdrone.domain.user.domain.User;
 import com.nolook.nolookdrone.domain.user.dto.UserCreateDto;
 import com.nolook.nolookdrone.domain.user.service.UserService;
+import com.nolook.nolookdrone.domain.user.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
 public class UserController {
     private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = new UserServiceImpl();
+    }
 
     @PostMapping("/")
     public ResponseEntity<Object> createUser(@RequestBody UserCreateDto userCreateDto) {
--- src/main/java/com/nolook/nolookdrone/domain/user/dto/UserCreateDto.java
+++ src/main/java/com/nolook/nolookdrone/domain/user/dto/UserCreateDto.java
@@ -1,11 +1,37 @@
 package com.nolook.nolookdrone.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class UserCreateDto {
     private String userId;
     private String userPassword;
+    private String userName;
+    private String address;
     private String phone;
     private String email;
+    private Integer age;
+    private Character sex;
+
+    public Boolean matchRegex(String col, String regex) {
+        return col.matches(regex);
+    }
+
+//    public Boolean idMatchRegex(String regex) {
+//        return userId.matches(regex);
+//    }
+//
+//    public Boolean pwdMatchRegex(String regex) {
+//        return userPassword.matches(regex);
+//    }
+//
+//    public Boolean phoneMatchRegex(String regex) {
+//        return phone.matches(regex);
+//    }
+//
+//    public Boolean emailMatchRegex(String regex) {
+//        return email.matches(regex);
+//    }
 }
